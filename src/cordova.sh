111257e4f9a060c8d0e334cc52c13daa9b338ae5
Marian-13 <Marian144519@gmail.com>
2019-04-24 02:23:44 +0300

    Install Cordova with sudo

--- src/cordova.sh
+++ src/cordova.sh
@@ -9,4 +9,4 @@ test `which node` || { echo '"cordova" failed' >> log.txt && return; }
 
 CORDOVA_VERSION=$1 # '9.0.0'
 
-npm install -g cordova@$CORDOVA_VERSION
+sudo npm install -g cordova@$CORDOVA_VERSION
